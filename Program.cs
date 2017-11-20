7e9f13426d975e957d536fad6948c8fafbc0c06a
Sanjay Dwivedi <sanjaydwivedi04@gmail.com>
2017-11-20 11:16:34 +0530

    Update Program.cs
    Updated code

--- Program.cs
+++ Program.cs
@@ -18,23 +18,10 @@ namespace SolidPrincipile
             }
             Console.WriteLine(revs + "=" + s);
         }
-        static void Anagram()
-        {
-            Console.WriteLine("Enter Word 1");
-            string word1 = Console.ReadLine();
-            Console.WriteLine("Enter Word 2");
-            string word2 = Console.ReadLine();
-            char[] char1 = word1.ToCharArray();
-            char[] char2 = word2.ToCharArray();
-            Array.Sort(char1);
-            Array.Sort(char2);
-            string newword1 = new string(char1);
-            string newword2 = new string(char2);
-            Console.WriteLine(newword1 + "" + newword2);
-        }
+        
         static void Main(string[] args)
         {
-            Anagram();
+            Palindrome();
             Console.Read();
         }
     }
